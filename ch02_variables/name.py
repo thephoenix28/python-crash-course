6893cb16d11a41e39eac78d8127a292b7cd5fa55
thephoenix28 <billyhodge9@gmail.com>
2026-03-09 14:00:00 -0400

    Ch02: practice with string methods - title, upper, lower, f-strings

---
+++ ch02_variables/name.py
@@ -0,0 +1,22 @@
+name = "ada lovelace"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "Ada"
+last_name = "Lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+first_name = "Ada"
+last_name = "Lovelace"
+full_name = f"{first_name} {last_name}"
+print(f"Hello, {full_name.title()}!")
+
+print("Python")
+print("\tPython")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
