0063c984d65ef2bb5c0c79d29d4e3e9b8c2adf72
thephoenix28 <billyhodge9@gmail.com>
2026-03-10 15:00:00 -0400

    Ch03: intro to lists - accessing elements and printing names

---
+++ ch03_lists/names.py
@@ -0,0 +1,10 @@
+names = ['Mom', 'Dad', 'Bella', 'Nestor', 'Abi']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+print(names[3].title())
+print(names[4].title())
+
+names = ['Mom', 'Dad', 'Bella', 'Nestor', 'Abi']
+message = f"These are my friends and family: {', '.join(names)}"
+print(message)
